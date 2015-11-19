7806bc6e78e3400954c09b6e76c1f7f501fe0a17
BjoernZimmermann <bjconcepts.de@gmaill.com>
2015-11-19 17:36:45 +0100

    MenuBar
    Deklaration von MenuBar für Editor-Applikation. Kommentare werden
    nachträglich eingefügt.

---
+++ src/de/hdm/tellme/client/gui/MenuBar.java
@@ -0,0 +1,47 @@
+package de.hdm.tellme.client.gui;
+
+ 
+ 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.RootPanel;
+
+ 
+public class MenuBar {
+
+	// Kommentar nachtragen
+	private HorizontalPanel menuBarPanel = new HorizontalPanel(); 
+	private Button nachrichtBtn = new Button(); 
+	private Button unterhatlungBtn = new Button(); 
+	private Button aboverwaltungBtn = new Button(); 
+	private Button profilBtn = new Button(); 
+	private Image logo = new Image();
+
+
+	public void onLoad() {
+		
+		// Kommentar nachtragen
+		logo.setWidth("70px");
+		// image.setUrl('http://127.0.0.1:8888/images/accounts.png');
+		
+		// Kommentar nachtragen
+		menuBarPanel.addStyleName("menuBarPanel");
+		nachrichtBtn.addStyleName("nachrichtBtn");
+		unterhatlungBtn.addStyleName("unterhatlungBtn");
+		aboverwaltungBtn.addStyleName("aboverwaltungBtn");
+		profilBtn.addStyleName("profilBtn");
+		logo.addStyleName("logo");
+	
+		// Kommentar nachtragen
+		menuBarPanel.add(nachrichtBtn);
+		menuBarPanel.add(unterhatlungBtn);
+		menuBarPanel.add(aboverwaltungBtn);
+		menuBarPanel.add(profilBtn);
+		
+		// Kommentar nachtragen
+		RootPanel.get().add(menuBarPanel);
+		
+	}
+
+}
