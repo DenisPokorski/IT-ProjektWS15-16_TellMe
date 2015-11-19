5748026f7079979d204d27fe91d3ee6cb98303f7
DanaThuering <dt018@hdm-stuttgart.de>
2015-11-19 18:36:15 +0100

    Mein Profil GUI
    Deklaration von GUI Elementen (Labels, Textboxen, Buttons).
    Hinzufügen von CSS-Stylennamen von GUI Elementen. Kommentare werden
    nachgetragen.

--- src/de/hdm/tellme/client/TellMe.java
+++ src/de/hdm/tellme/client/TellMe.java
@@ -1,24 +1,61 @@
 package de.hdm.tellme.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class TellMe implements EntryPoint {
+	
+	//Kommentar nachtragen(TODO)
+	private VerticalPanel profilPanel = new VerticalPanel(); 
+	 
+	private Label ueberschrift1 = new Label("Mein Profil");
+	private Label untertitel = new Label("todo");
+	private Label ueberschrift2 = new Label("Profil bearbeiten");
+	
+	//Kommentar nachtragen(TODO)
+	private TextBox vornameTextBox = new TextBox(); 
+	private TextBox nachnameTextBox = new TextBox(); 
+	private TextBox emailTextBox = new TextBox(); 
+
+	//Kommentar nachtragen(TODO)
+	private Button aenderungenSpeichernButton = new Button();
+	private Button profilAbmeldenButton = new Button();
+	private Button profilloeschenButton = new Button();
+
+
+
 
 	public void onModuleLoad() {
+		
+		//Kommentar nachtragen(TODO)
+		profilPanel.add(ueberschrift1);
+		profilPanel.add(untertitel);
+		profilPanel.add(ueberschrift2);
+		
+		profilPanel.add(vornameTextBox);
+		profilPanel.add(emailTextBox);
+		profilPanel.add(aenderungenSpeichernButton);
+		profilPanel.add(profilAbmeldenButton);
+		profilPanel.add(profilloeschenButton);
+
+		//Kommentar nachtragen(TODO)
+		ueberschrift1.addStyleName("ueberschrift1"); 
+		ueberschrift1.addStyleName("untertitel"); 
+		ueberschrift1.addStyleName("ueberschrift2"); 
+		ueberschrift1.addStyleName("vornameTextBox"); 
+		ueberschrift1.addStyleName("emailTextBox"); 
+		ueberschrift1.addStyleName("aenderungenSpeichernButton"); 
+		ueberschrift1.addStyleName("profilAbmeldenButton"); 
+		ueberschrift1.addStyleName("profilloeschenButton"); 
+
+		
+		//Kommentar nachtragen(TODO)
+		RootPanel.get("content").add(profilPanel);
+		
 
 	}
 }
