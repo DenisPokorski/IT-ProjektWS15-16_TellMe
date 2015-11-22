0d4149a03d65a944bcef63d9c26285d0929600b9
DanaThuering <dt018@hdm-stuttgart.de>
2015-11-22 10:55:34 +0100

    Buttonbeschreibungen
    Verbesserung und Ergänzungen der Buttons

--- src/de/hdm/tellme/client/TellMe.java
+++ src/de/hdm/tellme/client/TellMe.java
@@ -13,18 +13,21 @@ public class TellMe implements EntryPoint {
 	private VerticalPanel profilPanel = new VerticalPanel(); 
 	 
 	private Label ueberschrift1 = new Label("Mein Profil");
-	private Label untertitel = new Label("todo");
+	private Label untertitel = new Label("Hier siehst du dein Profil und kannst es bearbeiten, löschen und dich abmelden.");
 	private Label ueberschrift2 = new Label("Profil bearbeiten");
 	
 	//Kommentar nachtragen(TODO)
+	private Label bezeichnungVornameTextBox = new Label("Vorname"); 
 	private TextBox vornameTextBox = new TextBox(); 
+	private Label bezeichnungNachnameTextBox = new Label("Nachname"); 
 	private TextBox nachnameTextBox = new TextBox(); 
+	private Label bezeichnungEmailTextBox = new Label("Emailadresse"); 
 	private TextBox emailTextBox = new TextBox(); 
 
 	//Kommentar nachtragen(TODO)
-	private Button aenderungenSpeichernButton = new Button();
-	private Button profilAbmeldenButton = new Button();
-	private Button profilloeschenButton = new Button();
+	private Button aenderungenSpeichernButton = new Button("Änderungen speichern");
+	private Button profilAbmeldenButton = new Button("Abmelden");
+	private Button profilloeschenButton = new Button("Profil löschen");
 
 
 
@@ -36,8 +39,13 @@ public class TellMe implements EntryPoint {
 		profilPanel.add(untertitel);
 		profilPanel.add(ueberschrift2);
 		
+		profilPanel.add(bezeichnungVornameTextBox);
 		profilPanel.add(vornameTextBox);
+		profilPanel.add(bezeichnungNachnameTextBox);
+		profilPanel.add(nachnameTextBox);
+		profilPanel.add(bezeichnungEmailTextBox);
 		profilPanel.add(emailTextBox);
+		
 		profilPanel.add(aenderungenSpeichernButton);
 		profilPanel.add(profilAbmeldenButton);
 		profilPanel.add(profilloeschenButton);
@@ -46,7 +54,9 @@ public class TellMe implements EntryPoint {
 		ueberschrift1.addStyleName("ueberschrift1"); 
 		ueberschrift1.addStyleName("untertitel"); 
 		ueberschrift1.addStyleName("ueberschrift2"); 
-		ueberschrift1.addStyleName("vornameTextBox"); 
+		ueberschrift1.addStyleName("vornameTextBox");
+		ueberschrift1.addStyleName("nachnameTextBox");
+
 		ueberschrift1.addStyleName("emailTextBox"); 
 		ueberschrift1.addStyleName("aenderungenSpeichernButton"); 
 		ueberschrift1.addStyleName("profilAbmeldenButton"); 
