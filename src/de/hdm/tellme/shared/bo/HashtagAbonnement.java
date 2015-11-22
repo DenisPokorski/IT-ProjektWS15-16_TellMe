526f6815668bf934f9edfa6107dcbfa3c00bf20d
D p <dp@Ds-MacBook-Pro.local>
2015-11-22 13:59:19 +0100

    BusinessObject HashtagAbonnement angelegt

---
+++ src/de/hdm/tellme/shared/bo/HashtagAbonnement.java
@@ -0,0 +1,16 @@
+package de.hdm.tellme.shared.bo;
+
+public class HashtagAbonnement {
+	
+	private Hashtag aboHashtag;
+
+	public Hashtag getAboHashtag() {
+		return aboHashtag;
+	}
+
+	public void setAboHashtag(Hashtag aboHashtag) {
+		this.aboHashtag = aboHashtag;
+	}
+	
+
+}
