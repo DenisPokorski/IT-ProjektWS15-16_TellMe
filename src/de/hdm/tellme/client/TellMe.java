c41127a6b8c71890b8f66a199deb881d8ff18e65
BjoernZimmermann <bjconcepts.de@gmaill.com>
2015-11-26 12:08:37 +0100

    MenuBarEditor
    Menüleiste mit Menüpunkten der Editor Applikation erstellt und
    kommentiert.

--- src/de/hdm/tellme/client/TellMe.java
+++ src/de/hdm/tellme/client/TellMe.java
@@ -1,29 +1,24 @@
 package de.hdm.tellme.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
-
 import de.hdm.tellme.shared.LoginInfo;
-
 
 public class TellMe implements EntryPoint {
 	private LoginInfo loginInfo = null;
-	public void ladeStartseite(){
+	public void ladeMenuBarEditor(){
 		 
 		//Startseite anzeigen
-		StartSeiteEditor Startseite = new StartSeiteEditor();
-		Startseite.setLoginInfo(loginInfo);
+		MenuBarEditor menuBar = new MenuBarEditor();
+		menuBar.setLoginInfo(loginInfo);
 		RootPanel.get("header").clear();
-		RootPanel.get("header").add(Startseite);
+		RootPanel.get("header").add(menuBar);
 	}
 
 	@Override
 	public void onModuleLoad() {
-		ladeStartseite();
+		ladeMenuBarEditor();
+		//ladeNachrichten();
 		
 	}
 }
