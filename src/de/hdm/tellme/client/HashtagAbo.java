93d971960d8933c815f3f50f44ff6559a616dc24
Denis Pokorski <dp040@hdm-stuttgart.de>
2015-12-13 12:35:48 +0100

    CSS und Html ergänzt
    CSS:  #content_left definiert das Spalten content left und right
    nebeneinander sind
    
    HTML: content left und right bereich hinzugefügt für
    nachrichtenverwaltung
    
    Hashtagabo: Leserlichkeit verbessert

--- src/de/hdm/tellme/client/HashtagAbo.java
+++ src/de/hdm/tellme/client/HashtagAbo.java
@@ -17,7 +17,7 @@ import de.hdm.tellme.shared.LoginInfo;
 import de.hdm.tellme.shared.bo.Hashtag;
 
 public class HashtagAbo {
-	
+
 	/**
 	 * 
 	 * @author Denis Pokorski
@@ -25,28 +25,28 @@ public class HashtagAbo {
 	 * @since 26.11.2015
 	 * 
 	 */
-	
+
 	private final EditorServiceAsync asyncObj = GWT.create(EditorService.class);
 
 	private ListBox dropDownHashtagBereitsAbonniert = new ListBox();
 	private ListBox dropDownHashtagNochNichtAbooniert = new ListBox();
-	
+
 	private int auswahlIdHashtagAboLoeschen;
 
 	private LoginInfo loginInfo;
-	
-	
-	
-	public Button HashtagAboLoeschenButton ()	{
-		
+
+	public Button HashtagAboLoeschenButton() {
+
 		Button HashtagAboLoeschenBtn = new Button("Hashtagabnonnement löschen");
 		final int meineId = 7;
-		
+
 		HashtagAboLoeschenBtn.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event)	{
-				
-				if((auswahlIdHashtagAboLoeschen !=0) && (dropDownHashtagBereitsAbonniert.getSelectedIndex() > 0 )) {
-						HashtagAboLoeschenByIds(meineId, auswahlIdHashtagAboLoeschen);
+			public void onClick(ClickEvent event) {
+
+				if ((auswahlIdHashtagAboLoeschen != 0)
+						&& (dropDownHashtagBereitsAbonniert.getSelectedIndex() > 0)) {
+					HashtagAboLoeschenByIds(meineId,
+							auswahlIdHashtagAboLoeschen);
 				} else {
 					Window.alert("Bitte wählne Sie ein Hashtagabonnement in der Liste aus, dass gelöscht werden soll.");
 				}
@@ -54,66 +54,69 @@ public class HashtagAbo {
 		});
 		return HashtagAboLoeschenBtn;
 	}
-	
+
 	public void HashtagAboLoeschenByIds(int NutzerId, int HashtagId) {
-		asyncObj.loescheNutzeraboById(NutzerId, HashtagId, new AsyncCallback <Void>() {
-		@Override
-		public void onFailure(Throwable caught) {
-			Window.alert("Das Hashtagabo wurde nicht erfolgreich gelöscht.");
-			
-		}
-		@Override 
-		public void onSuccess(Void result) {
-			Window.alert("Das Hashtagabonnement wurde erfolgreich gelöscht.");
-			RootPanel.get("content").clear();
-			AboverwaltungEditor aB = new AboverwaltungEditor();
-			RootPanel.get("content").add(aB);
-		}
-	});
-		
+		asyncObj.loescheNutzeraboById(NutzerId, HashtagId,
+				new AsyncCallback<Void>() {
+					@Override
+					public void onFailure(Throwable caught) {
+						Window.alert("Das Hashtagabo wurde nicht erfolgreich gelöscht.");
+
+					}
+
+					@Override
+					public void onSuccess(Void result) {
+						Window.alert("Das Hashtagabonnement wurde erfolgreich gelöscht.");
+						RootPanel.get("content").clear();
+						AboverwaltungEditor aB = new AboverwaltungEditor();
+						RootPanel.get("content").add(aB);
+					}
+				});
+
 	}
-	
-	public void HashtagAboErstellenByIds(int NutzerId, int HashtagId)	{
+
+	public void HashtagAboErstellenByIds(int NutzerId, int HashtagId) {
 		asyncObj.erstellenHashtagAboById(NutzerId, HashtagId,
-				new AsyncCallback<Void>(){
-				@Override
-				public void onFailure(Throwable caught){
+				new AsyncCallback<Void>() {
+					@Override
+					public void onFailure(Throwable caught) {
 						Window.alert("Das Hashtagabo wurde nicht erstellt.");
-						
-				}
-				@Override
-				public void onSuccess(Void result){
-					Window.alert("Das Hashtagabonnement wurde erfolgreich erstellt.");
-					RootPanel.get("content").clear();
-					AboverwaltungEditor aE = new AboverwaltungEditor();
-					RootPanel.get("content").add(aE);
-				}
-		});
+
+					}
+
+					@Override
+					public void onSuccess(Void result) {
+						Window.alert("Das Hashtagabonnement wurde erfolgreich erstellt.");
+						RootPanel.get("content").clear();
+						AboverwaltungEditor aE = new AboverwaltungEditor();
+						RootPanel.get("content").add(aE);
+					}
+				});
 	}
-	
-	public ListBox getAbonnerteHashtagAboLoeschenListe(){
 
-		final int meineId=7;
-		
-		asyncObj.getZuAbonnierendeLoeschenHashtagAboListe(meineId, new AsyncCallback <Vector<Hashtag>>(){
+	public ListBox getAbonnerteHashtagAboLoeschenListe() {
 
-			@Override
-			public void onFailure(Throwable caught) {
-				// TODO Auto-generated method stub
-				
-			}
+		final int meineId = 7;
 
-			@Override
-			public void onSuccess(Vector<Hashtag> resultListe) {
-				// TODO Auto-generated method stub
-				
-			}
-		
-			
-		});
+		asyncObj.getZuAbonnierendeLoeschenHashtagAboListe(meineId,
+				new AsyncCallback<Vector<Hashtag>>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onSuccess(Vector<Hashtag> resultListe) {
+						// TODO Auto-generated method stub
+
+					}
+
+				});
 		return dropDownHashtagBereitsAbonniert;
 	}
-	
+
 	public void setLoginInfo(LoginInfo loginInfo) {
 		this.loginInfo = loginInfo;
 	}
