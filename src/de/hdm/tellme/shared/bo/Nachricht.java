515d014ebe78490465c0ca59d8518b9ba67e353f
DenisFeltrin <d.feltrin@icloud.com>
2015-12-22 11:15:17 +0100

    nachricht bo angepasst

--- src/de/hdm/tellme/shared/bo/Nachricht.java
+++ src/de/hdm/tellme/shared/bo/Nachricht.java
@@ -17,32 +17,32 @@ public class Nachricht extends BusinessObject {
 	 * Die ID ist nötig, damit die Klasse serialisierbar ist
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
-	 * Die Variable text ist vom Typ String
-	 * Die Variable senderId ist vom Typ Integer
-	 * Die Variable senderVorname ist vom Typ String
-	 * Die Variable senderNachname ist vom Typ String
-	 * Die Variable empfaengerListe ist vom Typ Vector<Nutzer>
+	 * Die Variable text ist vom Typ String Die Variable senderId ist vom Typ
+	 * Integer Die Variable senderVorname ist vom Typ String Die Variable
+	 * senderNachname ist vom Typ String Die Variable empfaengerListe ist vom
+	 * Typ Vector<Nutzer>
 	 */
 
 	private String text;
+	private Nutzer nutzer;
 	private int senderId;
-	private String senderVorname;
-	private String senderNachname;
 	private Vector<Nutzer> empfaengerListe;
-	
+
 	/**
 	 * Mit dieser Methode wird der Text ausgelesen
+	 * 
 	 * @return text
 	 */
 
 	public String getText() {
 		return text;
 	}
-	
+
 	/**
 	 * Mit dieser Methode wird der Text zugewiesen
+	 * 
 	 * @param text
 	 */
 
@@ -52,74 +52,68 @@ public class Nachricht extends BusinessObject {
 
 	/**
 	 * Mit dieser Methode wird die senderId ausgelesen
+	 * 
 	 * @return senderId
 	 */
-	
+
 	public int getSenderId() {
 		return senderId;
 	}
-	
+
 	/**
 	 * Mit dieser Methode wird die senderId zugewiesen
+	 * 
 	 * @param senderId
 	 */
 
 	public void setSenderId(int senderId) {
 		this.senderId = senderId;
 	}
-	
+
 	/**
 	 * Mit dieser Methode wird die empfaengerListe ausgelesen
+	 * 
 	 * @return empfaengerListe
 	 */
 
 	public Vector<Nutzer> getEmpfaengerListe() {
 		return empfaengerListe;
 	}
-	
+
 	/**
 	 * Mit dieser Methode wird die empfaengerListe zugewiesen
+	 * 
 	 * @param empfaengerListe
 	 */
 
 	public void setEmpfaengerListe(Vector<Nutzer> empfaengerListe) {
 		this.empfaengerListe = empfaengerListe;
 	}
-	
+
 	/**
 	 * Mit dieser Methode wird der senderVorname ausgelesen
+	 * 
 	 * @return senderVorname
 	 */
 
-	public String getSenderVorname() {
-		return senderVorname;
-	}
-	
 	/**
 	 * Mit dieser Methode wird der senderVorname zugewiesen
+	 * 
 	 * @param senderVorname
 	 */
 
-	public void setSenderVorname(String senderVorname) {
-		this.senderVorname = senderVorname;
-	}
-	
-	/**
-	 * Mit dieser Methode wird der senderNachname ausgelesen
-	 * @return senderNachname
-	 */
-
-	public String getSenderNachname() {
-		return senderNachname;
-	}
-	
 	/**
 	 * Mit dieser Methode wird der senderNachname zugewiesen
+	 * 
 	 * @param senderNachname
 	 */
 
-	public void setSenderNachname(String senderNachname) {
-		this.senderNachname = senderNachname;
+	public Nutzer getNutzer() {
+		return nutzer;
+	}
+
+	public void setNutzer(Nutzer nutzer) {
+		this.nutzer = nutzer;
 	}
 
 }
