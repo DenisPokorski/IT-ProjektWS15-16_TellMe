ba16dce70642a3388fcdd56cef472c078f5b12a7
DenisFeltrin <d.feltrin@icloud.com>
2015-12-22 11:11:20 +0100

    unterhaltungs bo

--- src/de/hdm/tellme/shared/bo/Unterhaltung.java
+++ src/de/hdm/tellme/shared/bo/Unterhaltung.java
@@ -2,43 +2,33 @@ package de.hdm.tellme.shared.bo;
 
 import java.util.Vector;
 
-public class Unterhaltung extends BusinessObject{
-	
+public class Unterhaltung extends BusinessObject {
 
-	private int typ;
-	private int sichtbarkeit;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Vector<Nachricht> alleNachrichten;
+	private Vector<Nutzer> teilnehmer;
 
-
-	public int getTyp() {
-		return typ;
+	public enum UnterhaltungsTyp {
+		privat, oeffentlich;
 	}
 
-	public void setTyp(int typ) {
-		this.typ = typ;
+	public Vector<Nachricht> getAlleNachrichten() {
+		return alleNachrichten;
 	}
-	
-//	
-//	private Vector <Nutzer> teilnehmerListe;
-//	private Vector <Nachricht> nachrichtListe;
-//	private Nutzer initiator;
-//	
-//	public Vector<Nutzer> getTeilnehmerListe() {
-//		return teilnehmerListe;
-//	}
-//	public void setTeilnehmerListe(Vector<Nutzer> teilnehmerListe) {
-//		this.teilnehmerListe = teilnehmerListe;
-//	}
-//	public Vector<Nachricht> getNachrichtListe() {
-//		return nachrichtListe;
-//	}
-//	public void setNachrichtListe(Vector<Nachricht> nachrichtListe) {
-//		this.nachrichtListe = nachrichtListe;
-//	}
-//	public Nutzer getInitiator() {
-//		return initiator;
-//	}
-//	public void setInitiator(Nutzer initiator) {
-//		this.initiator = initiator;
-//	}
+
+	public void setAlleNachrichten(Vector<Nachricht> alleNachrichten) {
+		this.alleNachrichten = alleNachrichten;
+	}
+
+	public Vector<Nutzer> getTeilnehmer() {
+		return teilnehmer;
+	}
+
+	public void setTeilnehmer(Vector<Nutzer> teilnehmer) {
+		this.teilnehmer = teilnehmer;
+	}
 
 }
