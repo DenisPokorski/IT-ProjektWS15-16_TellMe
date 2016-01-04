a87881c055c10e6d8d9f5fc36fb0b61362b4cc7a
Denis Pokorski <dp040@hdm-stuttgart.de>
2016-01-04 19:31:11 +0100

    Optische Änderungen
    NutzerAbo verbessert
    Neue Klasse ReportWillkommenSeite abgeändert
    Png Kreuz Bild eingepflegt

--- src/de/hdm/tellme/client/gui/editor/NutzerFormular.java
+++ src/de/hdm/tellme/client/gui/editor/NutzerFormular.java
@@ -16,12 +16,24 @@ package de.hdm.tellme.client.gui.editor;
  * the License.
  */
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.datepicker.client.DateBox;
 
 import de.hdm.tellme.shared.bo.Nutzer;
 
@@ -31,10 +43,8 @@ import de.hdm.tellme.shared.bo.Nutzer;
 
 public class NutzerFormular extends Composite {
 	private Nutzer nutzer = null;
-	Button btnAbonieren = new Button("abonnieren");
-	Button btnDeabonieren = new Button("deabonnieren");
-	
-	
+	Button btnAbonieren = new Button("abonieren");
+	Button btnDeabonieren = new Button("deabonieren");
 
 	public NutzerFormular() {
 		// Handle events.
@@ -56,14 +66,34 @@ public class NutzerFormular extends Composite {
 			}
 		});
 	}
-	
-	public VerticalPanel gibFormular(){
+
+	public VerticalPanel gibFormular() {
 		VerticalPanel vpForm = new VerticalPanel();
-		
-		vpForm.add(new Label(nutzer.getVorname() ));
-		vpForm.add(btnAbonieren);
-		vpForm.add(btnDeabonieren);
-		
+
+		HTML headline = new HTML(" <div class='" + "subline"
+				+ "'><h2>Nuterabo(s) verwalten:</h2></div> ");
+		vpForm.add(headline);
+
+		HTML subtext = new HTML(
+				" <div class='"
+						+ "subtext"
+						+ "'><h4>Hier können Sie andere Nutzer abonieren. Wenn Sie dies getan haben, werden alle öffentlichen und privaten nachrichten in den Sie als Empfänger asugewählt sind, in dem Menüpunkt Neuigkeiten angezeigt. <br /> Zudem können Sie bereits vorhandene abonnoments löschen. </h4></div> ");
+		vpForm.add(subtext);
+
+		Label selctionLabel = new Label("Ausgewählter Nutzer: "
+				+ nutzer.getVorname());
+		selctionLabel.setStylePrimaryName("selectionLabel");
+		vpForm.add(selctionLabel);
+
+		HorizontalPanel hpForm = new HorizontalPanel();
+		btnAbonieren.setStylePrimaryName("btnPositiv");
+		btnDeabonieren.setStylePrimaryName("btnNegativ");
+
+		hpForm.add(btnAbonieren);
+		hpForm.add(btnDeabonieren);
+
+		vpForm.add(hpForm);
+
 		return vpForm;
 	}
 
--- src/de/hdm/tellme/client/gui/report/ReportWillkommenSeite.java
+++ src/de/hdm/tellme/client/gui/report/ReportWillkommenSeite.java
@@ -3,13 +3,15 @@ package de.hdm.tellme.client.gui.report;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-public class ReportWillkommenSeite extends VerticalPanel{
+public class ReportWillkommenSeite extends VerticalPanel {
 	VerticalPanel willkommenPanel = new VerticalPanel();
 	Label reportUeberschrift = new Label("REPORT");
 	HorizontalPanel anordnungsPanel = new HorizontalPanel();
@@ -22,81 +24,86 @@ public class ReportWillkommenSeite extends VerticalPanel{
 	Label report1Untertitel = new Label("Report 1");
 	Label report2Untertitel = new Label("Report 2");
 	Label report3Untertitel = new Label("Report 3");
-	Label report1Beschreibung= new Label("Der Report 1 gibt alle Nachrichten eines Nutzers in einen bestimmten Zeitraum, \n"
-			+ "alle Nachrichten eines Nutzers,\n alle Nachrichten in einem bestimmten Zeitraum"
-			+ " \n oder alle Nachrichten aus");
-	Label report2Beschreibung = new Label("Der Report 2 gibt alle du bist ne kleine missge eines Nutzers aus");
-	Label report3Beschreibung = new Label("Der Report 3 gibt alle Hashtagabonnements eines Nutzers aus");
-	
+	Label report1Beschreibung = new Label(
+			"Der Report 1 gibt alle Nachrichten eines Nutzers in einen bestimmten Zeitraum, \n"
+					+ "alle Nachrichten eines Nutzers,\n alle Nachrichten in einem bestimmten Zeitraum"
+					+ " \n oder alle Nachrichten aus");
+	Label report2Beschreibung = new Label(
+			"Der Report 2 gibt alle du bist ne kleine missge eines Nutzers aus");
+	Label report3Beschreibung = new Label(
+			"Der Report 3 gibt alle Hashtagabonnements eines Nutzers aus");
+
 	Button report1Button = new Button("Zu Report 1");
 	Button report2Button = new Button("Zu Report 2");
 	Button report3Button = new Button("Zu Report 3");
 
-	
-	public void onLoad(){
-		willkommenPanel.add(reportUeberschrift);
+	public void onLoad() {
+		HorizontalPanel hpHeadline = new HorizontalPanel();
+		hpHeadline.setStylePrimaryName("hpHeadline");
+
+		HTML headline = new HTML(" <div class='" + "reportUebersichtPanel"
+				+ "'><h2>Alle Neuigkeiten</h2></div> ");
+		willkommenPanel.add(headline);
+
 		willkommenPanel.add(anordnungsPanel);
+		willkommenPanel.setStylePrimaryName("reportUebersichtPanel");
+
 		anordnungsPanel.add(left_Panel);
 		anordnungsPanel.add(center_Panel);
 		anordnungsPanel.add(right_Panel);
-		
-	
 
-		
-			left_Panel.add(report1Untertitel);
-			left_Panel.add(report1Beschreibung);
-			left_Panel.add(report1Button);
-			
-			center_Panel.add(report2Untertitel);
-			center_Panel.add(report2Beschreibung);
-			center_Panel.add(report2Button);
-			
-			right_Panel.add(report3Untertitel);
-			right_Panel.add(report3Beschreibung);
-			right_Panel.add(report3Button);
-			
-			RootPanel.get("content").clear();
-			RootPanel.get("content").add(willkommenPanel);
-		
-			report1Button.addClickHandler(new ClickHandler() {
-				
-				@Override
-				public void onClick(ClickEvent event) {
-						 setzeInhalt(new Report1Gui());
-						
-					}
-				
-			});
-			
-			report2Button.addClickHandler(new ClickHandler() {
-				
-				@Override
-				public void onClick(ClickEvent event) {
-						 setzeInhalt(new Report2Gui());
-						
-					}
-				
-			});
-			
-			report3Button.addClickHandler(new ClickHandler() {
-				
-				@Override
-				public void onClick(ClickEvent event) {
-						 setzeInhalt(new Report3Gui());
-						
-					}
-				
-			});
-			
-		
-		
+		left_Panel.add(report1Untertitel);
+		left_Panel.add(report1Beschreibung);
+		left_Panel.add(report1Button);
+
+		center_Panel.add(report2Untertitel);
+		center_Panel.add(report2Beschreibung);
+		center_Panel.add(report2Button);
+
+		right_Panel.add(report3Untertitel);
+		right_Panel.add(report3Beschreibung);
+		right_Panel.add(report3Button);
+
+		RootPanel.get("content").clear();
+		RootPanel.get("content").add(willkommenPanel);
+
+		report1Button.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				setzeInhalt(new Report1Gui());
+
+			}
+
+		});
+
+		report2Button.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				setzeInhalt(new Report2Gui());
+
+			}
+
+		});
+
+		report3Button.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				setzeInhalt(new Report3Gui());
+
+			}
+
+		});
+
 	}
-	
+
 	public static void setzeInhalt(Widget ZuSetzendesPanel) {
 
 		RootPanel.get("content").clear();
 		RootPanel.get("content").add(ZuSetzendesPanel);
-		
+
 	}
 
 }
