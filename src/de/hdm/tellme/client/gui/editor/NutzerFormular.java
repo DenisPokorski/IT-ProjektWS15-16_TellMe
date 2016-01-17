0e29fd0edda1afc90fe503c527ab9c7e11ceb5d9
Alex Homann <ah153@hdm-stuttgart.de>
2016-01-17 17:03:36 +0100

    NutzerFormular kommentiert.

--- src/de/hdm/tellme/client/gui/editor/NutzerFormular.java
+++ src/de/hdm/tellme/client/gui/editor/NutzerFormular.java
@@ -27,16 +27,29 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import de.hdm.tellme.shared.bo.Nutzer;
 
 /**
- * A form used for editing contacts.
+ * Die Klasse <class>NutzerFormular</class> wird aufgerufen, wenn in der
+ * Nutzerabonnementverwaltung auf einen Nutzer geklickt wird. Bei Klick auf
+ * einen Nutzer wird dieses Formular geladen. Es wird eine Beschreibung für
+ * Nutzerabonnements dargestellt. Außerdem erscheinen Buttons um ein Abonnement
+ * hinzuzufügen oder zu löschen. Bei Klick auf die jeweiligen Buttons wird ein
+ * Nutzerabonnement hinzugefügt oder gelöscht.
  */
 
 public class NutzerFormular extends Composite {
+
 	private Nutzer nutzer = null;
+
+	/**
+	 * Die Buttons zum Abonnement hinzufügen und löschen werden erstellt.
+	 */
 	Button btnAbonieren = new Button("Abonnement hinzufügen");
 	Button btnDeabonieren = new Button("Abonnement löschen");
 
 	public NutzerFormular() {
 		// Handle events.
+		/**
+		 * Clickhandler für den abonieren-Button
+		 */
 		btnAbonieren.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				if (nutzer == null) {
@@ -46,6 +59,9 @@ public class NutzerFormular extends Composite {
 			}
 		});
 		// Handle events.
+		/**
+		 * Clickhandler für den abonnement löschen-Button
+		 */
 		btnDeabonieren.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				if (nutzer == null) {
@@ -56,6 +72,14 @@ public class NutzerFormular extends Composite {
 		});
 	}
 
+	/**
+	 * Die Methode <code>gibFormular()</code> gibt ein VerticalPanel zurück,
+	 * dass das ausgewählte Nutzer Objekt abonniert werden kann oder das
+	 * Abonnement gelöscht wird.
+	 * 
+	 * @return VerticalPanel mit dem gewünschten Inhalt, in diesem Fall Formular
+	 *         für ein Nutzerabonnement.
+	 */
 	public VerticalPanel gibFormular() {
 		VerticalPanel vpForm = new VerticalPanel();
 
@@ -63,12 +87,11 @@ public class NutzerFormular extends Composite {
 		btnAbonieren.setStylePrimaryName("btnPositiv");
 		btnDeabonieren.setStylePrimaryName("btnNegativ");
 
-		
 		Label selctionLabel = new Label("Ausgewählter Nutzer: "
-				+ nutzer.getVorname()+" "+ nutzer.getNachname());
+				+ nutzer.getVorname() + " " + nutzer.getNachname());
 		selctionLabel.setStylePrimaryName("selectionLabel");
 		vpForm.add(selctionLabel);
-		
+
 		hpForm.add(btnAbonieren);
 		hpForm.add(btnDeabonieren);
 
@@ -76,8 +99,15 @@ public class NutzerFormular extends Composite {
 
 		return vpForm;
 	}
-	
-	public VerticalPanel gibBeschreibung(){
+
+	/**
+	 * Die Methode <code>gibBeschreibung()</code> gibt ein VerticalPanel zurück,
+	 * dass das die Beschreibung für die Nutzerabonnements enthält.
+	 * 
+	 * @return VerticalPanel mit dem gewünschten Inhalt, in diesem Fall die
+	 *         Beschreibung des Nutzerabonnements.
+	 */
+	public VerticalPanel gibBeschreibung() {
 		VerticalPanel vpForm = new VerticalPanel();
 
 		HTML headline = new HTML(" <div class='" + "subline"
@@ -90,19 +120,27 @@ public class NutzerFormular extends Composite {
 						+ "'><h4>Hier kannst du andere Nutzer abonnieren. Danach werden alle öffentlichen und privaten Nachrichten die an dich vom abonnierten Nutzer gesendet wurden, im Menüpunkt Neuigkeiten angezeigt. <br /> Zudem kannst du bereits vorhandene Abonnements löschen. </h4></div> ");
 		vpForm.add(subtext);
 
-		
 		return vpForm;
 	}
-/**
- * TODO ein button davon sollte immer grau sein sodass nur der farbige angeklickt wird.
- * @param ZellenObjekt
- */
+
+	/**
+	 * TODO ein button davon sollte immer grau sein sodass nur der farbige
+	 * angeklickt wird.
+	 * 
+	 * @param ZellenObjekt
+	 */
+	/**
+	 * Hier wird ein Nutzerabonnement in der Datenbank gesetzt. Das ausgewählte
+	 * ZellenObjekt, wird als Parameter übergeben.
+	 * 
+	 * @param ZellenObjekt
+	 */
 	public void setzeNutzerAbo(NutzerZelle.ZellenObjekt ZellenObjekt) {
 		this.nutzer = ZellenObjekt.nutzer;
 		if (ZellenObjekt.aboniert) {
 			btnAbonieren.setEnabled(false);
 			btnDeabonieren.setEnabled(true);
-			btnAbonieren.setStylePrimaryName("btnPositivBereitsAbooniert"); 
+			btnAbonieren.setStylePrimaryName("btnPositivBereitsAbooniert");
 
 		} else {
 			btnAbonieren.setEnabled(true);
