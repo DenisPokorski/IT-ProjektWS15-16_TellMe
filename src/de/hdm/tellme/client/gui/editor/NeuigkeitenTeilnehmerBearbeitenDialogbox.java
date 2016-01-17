6e32b4b2b7f174bbcc13aa7c7f1f19acfe455b6b
Denis Pokorski <dp040@hdm-stuttgart.de>
2016-01-17 17:06:18 +0100

    NueigkeitenTeilnehmerBearbeitenDialogbox kommentiert

--- src/de/hdm/tellme/client/gui/editor/NeuigkeitenTeilnehmerBearbeitenDialogbox.java
+++ src/de/hdm/tellme/client/gui/editor/NeuigkeitenTeilnehmerBearbeitenDialogbox.java
@@ -25,27 +25,64 @@ import de.hdm.tellme.shared.EditorServiceAsync;
 import de.hdm.tellme.shared.bo.Nutzer;
 import de.hdm.tellme.shared.bo.Unterhaltung;
 
+/**
+ * Die Klasse <class> NeuigkeitenTeilnehmerBearbeitenDialogbox</class> zeigt ein
+ * Dialogfenster an zum hinzufügen und entfernen von Teilnehmern
+ * 
+ * @author denispokorski
+ *
+ */
 public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 
+	/**
+	 * Eine neues Dialogbox-Fenster wird erstellt
+	 */
 	final DialogBox db = new DialogBox();
 
+	/**
+	 * Asyncobj wird erstellt.
+	 *
+	 */
 	private final EditorServiceAsync asyncObj = GWT.create(EditorService.class);
 
-	// Ausgelagert, da sie durch einen async call gefüllt werden
+	/**
+	 * Eine neue MultiWordSuggestionBox wird erstellt. Zwei neue Vektor-Listen
+	 * für mögliche Empfänger und ausgewählte Empfänger werden erstellt.
+	 * 
+	 * 
+	 */
+	/**
+	 * Ausgelagert, da sie durch einen async call gefüllt werden
+	 * 
+	 */
 	final MultiWordSuggestOracle suggestOracleEmpfaenger = new MultiWordSuggestOracle();
 	Vector<Nutzer> moeglicheEmpfaenger = new Vector<Nutzer>();
 	Vector<Nutzer> AusgewaehlteEmpfaenger = new Vector<Nutzer>();
 
-	// Panel für Nachricht Empfänger Dialog Box
+	/**
+	 * Panel für Nachricht Empfänger Dialog Box
+	 */
 	final FlowPanel fpAusgewaehlteEmpfanger = new FlowPanel();
 	HorizontalPanel hpEmpfaenger = new HorizontalPanel();
 
-	// Elemente, die von jedem Nachrichtemodus unterschiedlich befüllt werden
+	/**
+	 *  Elemente, die von jedem Nachrichtemodus unterschiedlich befüllt werden
+	 */
 	private String boxTitel;
 	private String textFunktionsbutton = "";
 
 	private Unterhaltung zuBearbeitendeUnterhaltung;
 
+	/**
+	 * Die Methode <code>getTeilnehmerBearbeiten </code> nimmt die ausgewählte
+	 * Unterhaltung/Nachricht und lädt die Teilnehmer die zum hinzufügen
+	 * vorhanden sind. Die zur Auwahl stehenden Teilnehmer werden hinzugefügt.
+	 * Buttons zum hinzufügen, entfernen und zum schließen der Dialogbox werden
+	 * hinzugefügt.
+	 * 
+	 * @param _unterhaltungZumBearbeiten
+	 * @return
+	 */
 	public DialogBox getTeilnehmerBearbeiten(
 			Unterhaltung _unterhaltungZumBearbeiten) {
 		boxTitel = "Teilnehmer bearbeiten";
@@ -56,6 +93,14 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 		for (Nutzer zuHinzuzufuegenderBenutzer : _unterhaltungZumBearbeiten
 				.getTeilnehmer()) {
 
+			/**
+			 * Ausgewählte Teilnehmer werden der Unterhaltung/Nachricht
+			 * hinzugefügt. Die bereits hinzugefügten Teilnehmer sind als Button
+			 * zum entfernen der Teilnehmer durch eine Stringerweiterung "(X)".
+			 * Dieser Button wird mittels CSS-Style optisch verändert.
+			 *
+			 */
+
 			AusgewaehlteEmpfaenger.addElement(zuHinzuzufuegenderBenutzer);
 			final Button btnLoescheEmpfaenger = new Button(
 					gibVorschlageTextFuerNutzer(zuHinzuzufuegenderBenutzer)
@@ -63,6 +108,9 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 			btnLoescheEmpfaenger
 					.setStylePrimaryName("ButtonX-Nachrichtenverwaltung");
 
+			/**
+			 * Ein ClickHandler um den Teilnehmer zu entfernen wird aufgerufen
+			 */
 			btnLoescheEmpfaenger.addClickHandler(new ClickHandler() {
 				public void onClick(ClickEvent event) {
 					// Wenn ein Benutzer in der
@@ -93,17 +141,24 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 	private DialogBox gibDialogBox() {
 		ladVorschlagListen();
 
-		// Dialog und FlowPanel definition
+		/**
+		 * Dialog und FlowPanel werden definiert
+		 */
 		db.setText(boxTitel);
 		db.setAnimationEnabled(true);
 		db.setGlassEnabled(true);
 
+		/**
+		 * Ein neues FlowPanel wird erstellt mit der Breite 100%
+		 */
 		FlowPanel fpDialog = new FlowPanel();
-		// fpDialog.setHeight("500px");
-		// fpDialog.setWidth("500px");
 
 		fpDialog.setWidth("100%");
 
+		/**
+		 * Ein neues Bild wird hinzugefügt der als Abbrech-Button fungiert. Ein
+		 * ClickHandler um die Dialogbox zu schließen wird aufgerufen.
+		 */
 		Image btnAbbrechen = new Image("xbtn.png");
 
 		btnAbbrechen.setStylePrimaryName("xbtn");
@@ -119,14 +174,30 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 		// ############################################## Empfaenger Panele
 		// ##############################################
 
+		/**
+		 * Eine neue SuggestBox wird hinzugefügt die eine Auswahl an
+		 * Teilnehmer,durch Multi-Word-Vorschläge, anzeigt, die hinzugefügt
+		 * werden können.
+		 * 
+		 * 
+		 */
 		final SuggestBox EmpfaengerHinzufuegenSug = new SuggestBox(
 				suggestOracleEmpfaenger);
 		hpEmpfaenger.add(EmpfaengerHinzufuegenSug);
 
+		/**
+		 * Es wird ein neuer Button "+ Empfänger" erstellt und mit einem
+		 * CSS-Style ergänzt
+		 */
 		final Button btnEmpfaengerHinzufuegen = new Button("+ Empfänger");
 		btnEmpfaengerHinzufuegen.setStylePrimaryName("EmpfaengerPlusBtn");
 		hpEmpfaenger.add(btnEmpfaengerHinzufuegen);
 
+		/**
+		 * Ein Clickhandler wird verwendet, um den Nutzer als Teilnehmer
+		 * hinzuzufügen.
+		 * 
+		 */
 		btnEmpfaengerHinzufuegen.addClickHandler(new ClickHandler() {
 
 			public void onClick(ClickEvent event) {
@@ -197,12 +268,33 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 
 			}
 		});
+
+		/**
+		 * Dem FlowPanel "fpDialog" das Horizontalpanel "hpEmpfaenger" und ein
+		 * weiteres FlowPanel "fpAusgewaehlteEmpfaenger" hinzugefügt
+		 * 
+		 */
+
 		fpDialog.add(hpEmpfaenger);
 		fpDialog.add(fpAusgewaehlteEmpfanger);
+
+		/**
+		 * 
+		 * Ein neues HorizontalPanel "hpButtons" wird erstellt und auf der
+		 * rechten Seite positioniert mit 100% Breite.
+		 * 
+		 */
 
 		HorizontalPanel hpButtons = new HorizontalPanel();
 		hpButtons.setWidth("100%");
 		hpButtons.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
+
+		/**
+		 * 
+		 * Ein neuer Button "textFunktionsbuton" der den Namen "Speichern" in im
+		 * Dialogbox hat. Der Button wird mittels CSS optisch verändert. Ein
+		 * ClickHandler wird aufgerufen, um zu Speichern.
+		 */
 
 		Button btnFunktionsbutton = new Button(textFunktionsbutton);
 		btnFunktionsbutton.setStylePrimaryName("NeuigkeitenDialogboxSendenBtn");
@@ -241,12 +333,22 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 			}
 		});
 
+		/**
+		 * "btnFunktionsbutton" wird dem HorizontalPanel "hpButtones"
+		 * hinzugefügt. "hpButteons" wird dem FlowPanel "fpDialog"
+		 */
 		hpButtons.add(btnFunktionsbutton);
 		fpDialog.add(hpButtons);
 
 		db.setWidget(fpDialog);
 		return db;
 	}
+
+	/**
+	 * Die Methode <code>ladVorschlagListen() </code> ruft eine RPC-Methode auf,
+	 * die einen Vector vom Nutzer-Objekten zurückgibt.
+	 * 
+	 */
 
 	private void ladVorschlagListen() {
 
@@ -255,6 +357,12 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 			public void onFailure(Throwable caught) {
 			}
 
+			/**
+			 * 
+			 * Bei Erfolg werden durch Multi-Word-Vorschläge die potenziellen
+			 * Teilnehmer angezeigt.
+			 * 
+			 */
 			@Override
 			public void onSuccess(Vector<Nutzer> resultListe) {
 				moeglicheEmpfaenger = resultListe;
@@ -268,6 +376,16 @@ public class NeuigkeitenTeilnehmerBearbeitenDialogbox {
 
 	}
 
+	/**
+	 * 
+	 * Die Methode <code> gibVorschlageTextFuerNutzer </code> liefert vom Nutzer
+	 * den Vornamen, Nachnamen und die E-Mail-Adresse zurück, um Vorschläge von
+	 * Nutzern bei der Eingabe von einzelnen Buchstaben zurückzugeben.
+	 * 
+	 * @param _nutzer
+	 * @return _nutzer.getVorname(); _nutzer.getNachname();
+	 *         _nutzer.getMailadresse()
+	 */
 	private String gibVorschlageTextFuerNutzer(Nutzer _nutzer) {
 		return _nutzer.getVorname() + " " + _nutzer.getNachname() + " ("
 				+ _nutzer.getMailadresse() + ")";
