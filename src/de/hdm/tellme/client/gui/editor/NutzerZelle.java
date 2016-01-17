5a5176fd3c4c96fcbee2a88c2e8e19e273174fa3
Alex Homann <ah153@hdm-stuttgart.de>
2016-01-17 15:33:46 +0100

    NutzerZelle kommentiert

--- src/de/hdm/tellme/client/gui/editor/NutzerZelle.java
+++ src/de/hdm/tellme/client/gui/editor/NutzerZelle.java
@@ -5,17 +5,40 @@ import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 
 import de.hdm.tellme.shared.bo.Nutzer;
 /**
+ * Die Hilfs-Klasse <class>NutzerZelle</class> wird benötigt um ein
+ * ZellenElement der <class>NutzerCellList</class> auszuwählen. Hier befinden
+ * sich auch die Nested-Classes <class>ZellenObjekt</class>
+ * <class>ZellenElement</class>. Durch die Klasse
+ * <class>ZellenObjekt</class>kann festgestellt werden ob ein Nutzer abonniert
+ * ist.
  * 
- * TODO
  * @author denispokorski
  *
  */
 public class NutzerZelle  {
+	
+	/**
+	 * Dies ist die Nested-Class <class>ZellenObjekt</class>. Ihre Funktion
+	 * wurde bereits für die Klasse <class>NutzerZelle</class> beschrieben.
+	 * 
+	 * @author denispokorski
+	 *
+	 */
 	public class ZellenObjekt {
 		public Nutzer nutzer;
 		public boolean aboniert;
 	}
-
+	/**
+	 * Die Klasse <class>ZellenElement</class> ist eine Klasse die durch die
+	 * Klasse <class>Abstract Cell</class> erweitert wird. Sie hat die Aufgabe
+	 * die ZellenElemente darzustellen. Außerdem dient sie zur Erzeugung von
+	 * HTML-Code für die benutzerdefinierten Objekte. Daraufhin wird das Nutzer-Objekt
+	 * in eine HTML-Tabelle eingefügt. 
+	 * 
+	 * 
+	 * @author denispokorski
+	 *
+	 */
 	public class ZellenElement extends AbstractCell<NutzerZelle.ZellenObjekt> {
 		@Override
 		public void render(Context context, NutzerZelle.ZellenObjekt value, SafeHtmlBuilder sb) {
